179257c663d0c3cda87317feeea2ab7f9206b19c
deepanshu tiwari <ddepu11@gmail.com>
2023-11-27 12:12:43 +0530

    feat: native module to switch on/off  flash light

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,22 +1,63 @@
 import React from 'react';
-import {useState} from 'react';
-import {SafeAreaView, StatusBar, Text, Button} from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+  Pressable,
+  StyleSheet,
+} from 'react-native';
 import RTNCalculator from 'rtn-calculator/js/NativeCalculator';
 
 const App: () => JSX.Element = () => {
-  const [result, setResult] = useState<number | null>(null);
+  const switchOnFlash = () => {
+    console.log('On');
+    RTNCalculator?.switchOnFlashLight();
+  };
+
+  const switchOffFlash = () => {
+    console.log('Off');
+
+    RTNCalculator?.switchOffFlashLight();
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={style.safeAreaView}>
       <StatusBar barStyle={'dark-content'} />
-      <Text style={{marginLeft: 20, marginTop: 20}}>3+7={result ?? '??'}</Text>
-      <Button
-        title="Compute"
-        onPress={async () => {
-          const value = await RTNCalculator?.add(67, 4);
-          setResult(value ?? null);
-        }}
-      />
+      <View style={style.wrapper}>
+        <Pressable style={style.pressable} onPress={switchOnFlash}>
+          <Text style={style.buttonText}>Switch On Flash Light</Text>
+        </Pressable>
+
+        <Pressable style={style.pressable} onPress={switchOffFlash}>
+          <Text style={style.buttonText}>Switch Of Flash Light</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
+
+const style = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
+    display: 'flex',
+    backgroundColor: '#2B2A4C',
+  },
+  pressable: {
+    padding: 5,
+    borderWidth: 0.6,
+    borderColor: '#96EFFF',
+    marginBottom: 40,
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#FFFF',
+    fontWeight: '400',
+    letterSpacing: 1,
+  },
+});
 export default App;
--- RTNCalculator/android/src/main/java/com/rtncalculator/CalculatorModule.kt
+++ RTNCalculator/android/src/main/java/com/rtncalculator/CalculatorModule.kt
@@ -3,13 +3,37 @@ package com.rtncalculator
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.rtncalculator.NativeCalculatorSpec
+import android.hardware.camera2.CameraManager
+import android.content.Context
 
 class CalculatorModule(reactContext: ReactApplicationContext) : NativeCalculatorSpec(reactContext) {
 
+  private val context: ReactApplicationContext = reactContext
+
   override fun getName() = NAME
 
-  override fun add(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a + b)
+  lateinit var cameraManager: CameraManager
+  lateinit var cameraID: String
+
+  override fun switchOnFlashLight(){
+
+    try {
+      cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+      cameraID = cameraManager.cameraIdList[0]
+
+//      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+      cameraManager.setTorchMode(cameraID, true)
+//      }
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+  }
+
+
+  override fun switchOffFlashLight(){
+    cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    cameraID = cameraManager.cameraIdList[0]
+    cameraManager.setTorchMode(cameraID, false)
   }
 
   companion object {
